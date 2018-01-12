83079785b162cb9e8733914ee7ea6b55a931d314
jessica <Jess@Jessicas-MacBook-Pro.local>
2018-01-11 22:50:53 -0500

    working on 3-2

--- Lesson03-Green-Recycler-View/T03.02-Exercise-ViewHolder/app/src/main/java/com/example/android/recyclerview/GreenAdapter.java
+++ Lesson03-Green-Recycler-View/T03.02-Exercise-ViewHolder/app/src/main/java/com/example/android/recyclerview/GreenAdapter.java
@@ -48,8 +48,8 @@ public class GreenAdapter extends RecyclerView.Adapter<GreenAdapter.NumberViewHo
     // complete (7) Set the height and width to wrap_content
     // TODO (8) Align the TextView to the start of the parent
     // TODO (9) Center the TextView vertically in the layout
-    // TODO (10) Set the font family to monospace
-    // TODO (11) Set the text size to 42sp
+    // complete (10) Set the font family to monospace
+    // complete (11) Set the text size to 42sp
 
     private static final String TAG = GreenAdapter.class.getSimpleName();
 
